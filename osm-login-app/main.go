5fb05c384cc04a889db23d89c31a9ce7e62e0a74
Florin Badita-Nistor <baditaflorin@gmail.com>
2024-05-17 15:53:35 +0300

    Final Oauth Flow

--- osm-login-app/main.go
+++ osm-login-app/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
@@ -103,40 +103,114 @@ func makeAuthenticatedRequest(token *oauth2.Token) {
 
 	fmt.Printf("User Info: %v\n", userInfo)
 
-	// Example usage: create a note
-	createMapNote(token, 44.442813587452, 26.153984068774, "This is a test note")
+	// Example usage: create a node with amenity=drinking_water
+	createMapNode(token, 44.442813587452, 26.153984068774, "drinking_water")
 }
 
-func createMapNote(token *oauth2.Token, lat, lon float64, text string) {
+func createChangeset(token *oauth2.Token) (int, error) {
 	client := oauth2Config.Client(oauth2.NoContext, token)
-	endpointURL := "https://api.openstreetmap.org/api/0.6/notes.json"
+	endpointURL := "https://api.openstreetmap.org/api/0.6/changeset/create"
 
-	data := url.Values{}
-	data.Set("lat", fmt.Sprintf("%f", lat))
-	data.Set("lon", fmt.Sprintf("%f", lon))
-	data.Set("text", text)
+	xmlData := `
+		<osm>
+			<changeset>
+				<tag k="created_by" v="go-example"/>
+				<tag k="comment" v="Adding a drinking water node"/>
+			</changeset>
+		</osm>`
 
-	req, err := http.NewRequest("POST", endpointURL, strings.NewReader(data.Encode()))
+	req, err := http.NewRequest("PUT", endpointURL, bytes.NewBufferString(xmlData))
 	if err != nil {
-		log.Fatalf("Failed to create request: %v", err)
+		return 0, fmt.Errorf("failed to create request: %v", err)
 	}
 	req.Header.Set("Authorization", "Bearer "+token.AccessToken)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Type", "text/xml")
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Failed to create note: %v", err)
+		return 0, fmt.Errorf("failed to create changeset: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Failed to create note: status code %d", resp.StatusCode)
+		return 0, fmt.Errorf("failed to create changeset: status code %d", resp.StatusCode)
 	}
 
-	var result map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		log.Fatalf("Failed to decode response: %v", err)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, fmt.Errorf("failed to read response body: %v", err)
 	}
 
-	fmt.Printf("Note created: %v\n", result)
+	var changesetID int
+	fmt.Sscanf(string(body), "%d", &changesetID)
+
+	return changesetID, nil
+}
+
+func closeChangeset(token *oauth2.Token, changesetID int) error {
+	client := oauth2Config.Client(oauth2.NoContext, token)
+	endpointURL := fmt.Sprintf("https://api.openstreetmap.org/api/0.6/changeset/%d/close", changesetID)
+
+	req, err := http.NewRequest("PUT", endpointURL, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+token.AccessToken)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to close changeset: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to close changeset: status code %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func createMapNode(token *oauth2.Token, lat, lon float64, amenity string) {
+	changesetID, err := createChangeset(token)
+	if err != nil {
+		log.Fatalf("Failed to create changeset: %v", err)
+	}
+
+	client := oauth2Config.Client(oauth2.NoContext, token)
+	endpointURL := "https://api.openstreetmap.org/api/0.6/node/create"
+
+	xmlData := fmt.Sprintf(`
+		<osm>
+			<node changeset="%d" lat="%f" lon="%f">
+				<tag k="amenity" v="%s"/>
+			</node>
+		</osm>`, changesetID, lat, lon, amenity)
+
+	req, err := http.NewRequest("PUT", endpointURL, bytes.NewBufferString(xmlData))
+	if err != nil {
+		log.Fatalf("Failed to create request: %v", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+token.AccessToken)
+	req.Header.Set("Content-Type", "text/xml")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to create node: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Failed to create node: status code %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	fmt.Printf("Node created: %s\n", string(body))
+
+	if err := closeChangeset(token, changesetID); err != nil {
+		log.Fatalf("Failed to close changeset: %v", err)
+	}
 }
