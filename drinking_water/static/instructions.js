a6af83687f4233f092a9e18a1111430b8b6873f8
Florin Badita-Nistor <baditaflorin@gmail.com>
2024-05-21 03:10:41 +0300

    Fix #7

--- drinking_water/static/instructions.js
+++ drinking_water/static/instructions.js
@@ -8,9 +8,19 @@ const toggleInstructions = () => {
     document.getElementById('toggle-instructions').textContent = isHidden ? 'Hide Route Instructions' : 'Show Route Instructions';
 };
 
+// Function to toggle project info visibility
+const toggleProjectInfo = () => {
+    const projectInfo = document.getElementById('project-info');
+    const isHidden = projectInfo.style.display === 'none';
+    projectInfo.style.display = isHidden ? 'block' : 'none';
+    document.getElementById('toggle-project-info').textContent = isHidden ? 'Hide Project Info' : 'Show Project Info';
+};
+
+
 // Function to attach event listeners
 const attachEventListeners = () => {
     document.getElementById('toggle-instructions').addEventListener('click', toggleInstructions);
+    document.getElementById('toggle-project-info').addEventListener('click', toggleProjectInfo);
 };
 
 // Function to initialize the DOM
