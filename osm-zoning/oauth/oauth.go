c3b659f57fcbd1cd840cab16ad6cb6e09dfbbe3f
Florin Badita-Nistor <baditaflorin@gmail.com>
2024-06-15 02:05:00 +0300

    osm-zoning WORKING VERSION - one changeset per each change still

--- osm-zoning/oauth/oauth.go
+++ osm-zoning/oauth/oauth.go
@@ -2,7 +2,9 @@ package oauth
 
 import (
 	"encoding/gob"
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"osm-zoning/config"
@@ -60,8 +62,8 @@ func createWayRequest(changesetID int, nodes []int64, tags map[string]string) (*
 	return utils.CreateRequest("PUT", "https://api.openstreetmap.org/api/0.6/way/create", "text/xml", []byte(xmlData))
 }
 
-func updateWayRequest(changesetID int, wayID int64, tags map[string]string) (*http.Request, error) {
-	log.Printf("Creating update request for way %d in changeset %d with tags %v", wayID, changesetID, tags)
+func updateWayRequest(changesetID int, wayID int64, version int, tags map[string]string) (*http.Request, error) {
+	log.Printf("Creating update request for way %d in changeset %d with version %d and tags %v", wayID, changesetID, version, tags)
 	var tagsXML string
 	for key, value := range tags {
 		tagsXML += fmt.Sprintf(`<tag k="%s" v="%s"/>`, key, value)
@@ -69,25 +71,65 @@ func updateWayRequest(changesetID int, wayID int64, tags map[string]string) (*ht
 
 	xmlData := fmt.Sprintf(`
         <osm>
-            <way id="%d" changeset="%d">
+            <way id="%d" changeset="%d" version="%d">
                 %s
             </way>
-        </osm>`, wayID, changesetID, tagsXML)
+        </osm>`, wayID, changesetID, version, tagsXML)
 
 	log.Printf("XmlData:\n %v", xmlData)
 
 	return utils.CreateRequest("PUT", fmt.Sprintf("https://api.openstreetmap.org/api/0.6/way/%d", wayID), "text/xml", []byte(xmlData))
 }
 
+func getWayVersion(wayID int64, token *oauth2.Token) (int, error) {
+	client := Oauth2Config.Client(oauth2.NoContext, token)
+	url := fmt.Sprintf("https://api.openstreetmap.org/api/0.6/way/%d", wayID)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create request: %v", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+token.AccessToken)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, fmt.Errorf("request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return 0, fmt.Errorf("failed to get way version: status code %d and Body:%s", resp.StatusCode, string(body))
+	}
+
+	type OSM struct {
+		Way struct {
+			Version int `xml:"version,attr"`
+		} `xml:"way"`
+	}
+
+	var osmData OSM
+	if err := xml.NewDecoder(resp.Body).Decode(&osmData); err != nil {
+		return 0, fmt.Errorf("failed to parse response: %v", err)
+	}
+
+	return osmData.Way.Version, nil
+}
+
 func UpdateWayTags(cfg *config.Config, token *oauth2.Token, wayID int64, tags map[string]string) {
 	log.Printf("Updating way tags for way %d with tags %v", wayID, tags)
+
+	version, err := getWayVersion(wayID, token)
+	if err != nil {
+		log.Fatalf("Failed to get way version: %v", err)
+	}
+
 	changesetID, err := CreateChangeset(cfg, token)
 	if err != nil {
 		log.Fatalf("Failed to create changeset: %v", err)
 	}
 
 	client := Oauth2Config.Client(oauth2.NoContext, token)
-	req, err := updateWayRequest(changesetID, wayID, tags)
+	req, err := updateWayRequest(changesetID, wayID, version, tags)
 	if err != nil {
 		log.Fatalf("Failed to create request: %v", err)
 	}
